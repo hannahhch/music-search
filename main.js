53a14facf315f86dc76a80b39fb6395558a798b4
Hannah Hudson <hannah.hch@gmail.com>
2017-08-15 09:43:26 -0400

    added js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 //target the search box input value. append that to the itunes api
 
-//fetch itunes API 
+//fetch itunes API
+let button = document.querySelector('.search-button');
+
+let searchTunes = 'https://itunes.apple.com/search?term=key1=value1&key2=value2&key3-value3';
+
+button.addEventLIstener('click', function() {
+  var searchBox = document.querySelector('.search-music').value;
+  let searchURL = searchTunes + seachBox;
+
+
+fetch(searchURL)
+
+})
