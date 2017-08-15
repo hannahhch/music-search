e32d18454a50e4361ce0b87f8f529cbc8c46fb29
Hannah Hudson <hannah.hch@gmail.com>
2017-08-15 13:02:52 -0400

    able to fetch the api and get the input value

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-//target the search box input value. append that to the itunes api
 
-//fetch itunes API
+//get the submit button
 let button = document.querySelector('.search-button');
 
-let searchTunes = 'https://itunes.apple.com/search?term=key1=value1&key2=value2&key3-value3';
+//get the iTunes search 'api' and assign key values
+let searchTunes = 'https://itunes.apple.com/search?term=';
 
-button.addEventLIstener('click', function() {
+//when the button is clicked
+button.addEventListener('click', function() {
+  //get the value from the search music input box
   var searchBox = document.querySelector('.search-music').value;
-  let searchURL = searchTunes + seachBox;
+  //declare a variable that concatenates the API and search term
+  let searchURL = searchTunes + searchBox;
 
-
+//fetch the concatenation
 fetch(searchURL)
+
+  .then(
+    //if no response, return the error
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Error: ' + response.status);
+        return;
+        //if response is 200, it worked!
+      } console.log('it worked');
+
+
+      response.json().then(function(data) {
+        document.querySelector
+      })
+
+    }
+  )
 
 })
